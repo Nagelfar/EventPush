2318857ea40d85daead3b986f637299aad5dfb50
Christian Folie <christian.folie@gmail.com>
2014-12-08 15:46:09 +0100

    changed registration in handler

--- EventPush/Scripts/Handlers.js
+++ EventPush/Scripts/Handlers.js
@@ -2,28 +2,79 @@
 (function (EventPush) {
     (function (Handlers) {
         function registerChildActionRefresh(bus) {
+            var initialMessage = "";
+
             $('div[data-refresh-action]').each(function (index, elem) {
+                var dataRefresh = new DataRefresh(elem);
                 var attributes = elem.attributes;
-                for (var i = 0; i < attributes.length; i++) {
-                    var attribute = attributes.item(i);
-                    if (attribute.name.toLowerCase().indexOf('data-refresh-event-') >= 0) {
-                        var url = elem.attributes["data-refresh-action"];
 
-                        if (!url)
-                            throw "Cannot register a refresh handler for " + attribute.name + " with an empty url!";
+                var matches = $.map(attributes, function (attribute) {
+                    return dataRefresh.isMatchingEvent(attribute);
+                });
+                matches.filter(function (er) {
+                    return er != null;
+                }).filter(function (er) {
+                    return er.validateUrl();
+                }).forEach(function (er) {
+                    return er.registerHandler(bus);
+                });
 
-                        var messageElement = elem.attributes["data-refresh-message-" + attribute.value.toLowerCase()];
-
-                        var message = "Daten werden geladen!";
-                        if (messageElement && messageElement.value)
-                            message = messageElement.value;
-
-                        bus.registerHandler(attribute.value, refreshHandler(url.value, elem, message));
-                    }
-                }
+                if (attributes["data-refresh-message"])
+                    initialMessage = attributes["data-refresh-message"].value;
             });
+
+            if (initialMessage) {
+                toastr.info(initialMessage);
+            }
         }
         Handlers.registerChildActionRefresh = registerChildActionRefresh;
+
+        var DataRefresh = (function () {
+            function DataRefresh(elem) {
+                this.elem = elem;
+            }
+            DataRefresh.prototype.isMatchingEvent = function (attribute) {
+                if (attribute && attribute.name.toLowerCase().indexOf('data-refresh-event-') >= 0) {
+                    return new EventRefresh(this.elem, attribute);
+                }
+                return null;
+            };
+            return DataRefresh;
+        })();
+
+        var EventRefresh = (function () {
+            function EventRefresh(elem, attribute) {
+                this.elem = elem;
+                this.attribute = attribute;
+                this.attributes = elem.attributes;
+            }
+            EventRefresh.prototype.validateUrl = function () {
+                var url = this.attributes["data-refresh-action"];
+
+                if (!url)
+                    throw "Cannot register a refresh handler for " + this.attribute.name + " with an empty url!";
+
+                this.urlLink = url.value;
+                return true;
+            };
+
+            EventRefresh.prototype.extractMessage = function () {
+                var messageElement = this.attributes["data-refresh-message-" + this.attribute.value.toLowerCase()];
+
+                var message = "Daten werden geladen!";
+                if (messageElement && messageElement.value)
+                    message = messageElement.value;
+
+                return message;
+            };
+
+            EventRefresh.prototype.registerHandler = function (bus) {
+                var message = this.extractMessage();
+
+                bus.registerHandler(this.attribute.value, refreshHandler(this.urlLink, this.elem, message));
+            };
+            return EventRefresh;
+        })();
 
         function refreshHandler(url, element, message) {
             return function (event) {
--- EventPush/Scripts/Handlers.ts
+++ EventPush/Scripts/Handlers.ts
@@ -1,27 +1,72 @@
 ﻿module EventPush.Handlers {
 
     export function registerChildActionRefresh(bus: Bus) {
+        var initialMessage = "";        
+
         $('div[data-refresh-action]').each(function (index, elem) {
+            var dataRefresh = new DataRefresh(elem);
             var attributes = elem.attributes;
-            for (var i = 0; i < attributes.length; i++) {
-                var attribute = attributes.item(i);
-                if (attribute.name.toLowerCase().indexOf('data-refresh-event-') >= 0) {
+           
+            var matches: EventRefresh[] = $.map(attributes, (attribute) => dataRefresh.isMatchingEvent(attribute));
+            matches
+                .filter((er) => er != null)
+                .filter((er) => er.validateUrl())
+                .forEach((er) => er.registerHandler(bus));
 
-                    var url = elem.attributes["data-refresh-action"];
-
-                    if (!url)
-                        throw "Cannot register a refresh handler for " + attribute.name + " with an empty url!";
-
-                    var messageElement = elem.attributes["data-refresh-message-" + attribute.value.toLowerCase()];
-
-                    var message = "Daten werden geladen!";
-                    if (messageElement && messageElement.value)
-                        message = messageElement.value;
-
-                    bus.registerHandler(attribute.value, refreshHandler(url.value, elem, message));
-                }
-            }
+            if (attributes["data-refresh-message"])
+                initialMessage = attributes["data-refresh-message"].value;
         });
+
+        if (initialMessage) {            
+            toastr.info(initialMessage);            
+        }
+    }
+
+    class DataRefresh {
+        constructor(private elem: Element) {
+        }
+
+        public isMatchingEvent(attribute: Attr): EventRefresh {
+            if (attribute && attribute.name.toLowerCase().indexOf('data-refresh-event-') >= 0) {
+                return new EventRefresh(this.elem, attribute);
+            }
+            return null;
+        }
+
+    }
+
+    class EventRefresh {
+        attributes: NamedNodeMap;
+        urlLink: string;
+
+        constructor(private elem: Element, private attribute: Attr) {
+            this.attributes = elem.attributes;
+        }
+        public validateUrl():boolean {
+            var url = this.attributes["data-refresh-action"];
+
+            if (!url)
+                throw "Cannot register a refresh handler for " + this.attribute.name + " with an empty url!";
+
+            this.urlLink = url.value;
+            return true;
+        } 
+
+        private extractMessage(): string {
+            var messageElement = this.attributes["data-refresh-message-" + this.attribute.value.toLowerCase()];
+
+            var message = "Daten werden geladen!";
+            if (messageElement && messageElement.value)
+                message = messageElement.value;
+
+            return message;
+        }
+
+        public registerHandler(bus: Bus) {
+            var message = this.extractMessage();
+
+            bus.registerHandler(this.attribute.value, refreshHandler(this.urlLink, this.elem, message));
+        }
     }
 
 
