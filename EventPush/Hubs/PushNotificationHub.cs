e552f590440cc7a0895e7858eadb81f20ba080bd
Christian Folie <christian.folie@gmail.com>
2014-12-08 22:06:08 +0100

    connection mapping

--- EventPush/Hubs/PushNotificationHub.cs
+++ EventPush/Hubs/PushNotificationHub.cs
@@ -10,15 +10,27 @@ namespace EventPush.Hubs
 {
     public class PushNotificationHub : Hub
     {
+        //private readonly static System.Web.Caching.Cache cache = new System.Web.Caching.Cache();
+        private static ConnectionMapping<string> _userConnections = new ConnectionMapping<string>();
         //public void PushEvent<T>(T @event) where T : class
         //{
         //    this.Clients.All.reciveEvent(typeof(T).Name, @event);
         //}
+
+        private static IHubContext FindContext()
+        {
+            return GlobalHost.ConnectionManager.GetHubContext<PushNotificationHub>();
+        }
+
+        //public static void RegisterCorrelationId(IIdentity identity, string correlationId)
+        //{
+
+        //    context.Clients.Group(identity.Name);
+        //}
+
         public static void PushEvent<T>(IIdentity identity, T @event) where T : class,IEvent
         {
-            var context= GlobalHost.ConnectionManager.GetHubContext<PushNotificationHub>();
-
-            context.Clients.Group(identity.Name).reciveEvent(typeof(T).Name, @event);
+            FindContext().Clients.Group(identity.Name).reciveEvent(typeof(T).Name, @event);
         }
 
         public override System.Threading.Tasks.Task OnConnected()
@@ -27,7 +39,82 @@ namespace EventPush.Hubs
 
             Groups.Add(Context.ConnectionId, name);
 
+            _userConnections.Add(name, Context.ConnectionId);
+
             return base.OnConnected();
         }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            _userConnections.Remove(Context.User.Identity.Name, Context.ConnectionId);
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public class ConnectionMapping<T>
+        {
+            private readonly Dictionary<T, HashSet<string>> _connections =
+                new Dictionary<T, HashSet<string>>();
+
+            public int Count
+            {
+                get
+                {
+                    return _connections.Count;
+                }
+            }
+
+            public void Add(T key, string connectionId)
+            {
+                lock (_connections)
+                {
+                    HashSet<string> connections;
+                    if (!_connections.TryGetValue(key, out connections))
+                    {
+                        connections = new HashSet<string>();
+                        _connections.Add(key, connections);
+                    }
+
+                    lock (connections)
+                    {
+                        connections.Add(connectionId);
+                    }
+                }
+            }
+
+            public IEnumerable<string> GetConnections(T key)
+            {
+                HashSet<string> connections;
+                if (_connections.TryGetValue(key, out connections))
+                {
+                    return connections;
+                }
+
+                return Enumerable.Empty<string>();
+            }
+
+            public void Remove(T key, string connectionId)
+            {
+                lock (_connections)
+                {
+                    HashSet<string> connections;
+                    if (!_connections.TryGetValue(key, out connections))
+                    {
+                        return;
+                    }
+
+                    lock (connections)
+                    {
+                        connections.Remove(connectionId);
+
+                        if (connections.Count == 0)
+                        {
+                            _connections.Remove(key);
+                        }
+                    }
+                }
+            }
+        }
     }
+
 }
